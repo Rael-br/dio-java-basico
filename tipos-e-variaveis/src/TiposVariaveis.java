8083f2dc405b80386ca80fd74261ed4e5c4c7dc1
Rael-br <rael.souzachacon@gmail.com>
2024-06-11 09:54:03 -0300

    tipos e variaveis

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        //Estudar:
+        //Tipos primitivos 
+        //texto na aplicação 
+
+        String meuNome = "Israel Chacon Souza";
+        
         double salarioMinimo = 2500;
+
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
     }
 }
