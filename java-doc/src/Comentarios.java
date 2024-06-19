0ad0d8b54586d0087efba507a349edf52453c06c
Rael-br <rael.souzachacon@gmail.com>
2024-06-19 09:40:41 -0300

    19/06

---
+++ java-doc/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // (ONE LINE) Olá, eu sou um comentário em uma unica linha
+
+        /* (MULT LINE) Olá,
+        Eu sou um documentario
+        que posso ser mais detalhado 
+         quando necesaario*/
+
+         /** (DOCUMENTATION)
+          * Estas duas estrelinhas acima
+          * é para identificar que você
+          * pretende elaboorar um comentario 
+          * a nivel de documentação.
+          * Que incrivel !!! 
+          */
+        
+        
+    }
+    
+}
